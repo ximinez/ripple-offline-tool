fc7cbaaaa5389d8bd24cbb624bc365b2182a43a8
Ed Hennis <ed@ripple.com>
2026-01-09 13:39:45 -0400

    Add ability to hash arbitrary transaction, maybe malformed (#18)

--- src/OfflineTool.cpp
+++ src/OfflineTool.cpp
@@ -133,6 +133,17 @@ doDeserialize(std::string const& data)
 }
 
 int
+doHash(ripple::HashPrefix prefix, std::string const& data)
+{
+    auto const obj = offline::make_stobject(data);
+
+    auto const result = obj.getHash(prefix);
+
+    std::cout << result << std::endl;
+    return EXIT_SUCCESS;
+}
+
+int
 doSign(
     std::string const& data,
     boost::filesystem::path const& keyFile,
@@ -312,6 +323,10 @@ runCommand(
         BOOST_ASSERT(input);
         return doDeserialize(*input);
     };
+    auto const txhash = [](auto const& input, auto const&, auto const&) {
+        BOOST_ASSERT(input);
+        return doHash(ripple::HashPrefix::transactionID, *input);
+    };
     auto const sign = [](auto const& input, auto const& keyFile, auto const&) {
         BOOST_ASSERT(input);
         return doSingleSign(*input, keyFile);
@@ -338,6 +353,7 @@ runCommand(
         {"sign", {false, sign}},
         {"multisign", {false, multisign}},
         {"asign", {false, asign}},
+        {"txhash", {false, txhash}},
         {"createkeyfile", {true, createkeyfile}},
     };
 
@@ -406,6 +422,8 @@ printHelp(
       Output is unserialized JSON.
   Arbitrary signing:
     asign <argument>|--stdin            Sign arbitrary data.
+  Hashing:
+    txhash <argument>|--stdin           Hash a transaction.
   Key Management:
     createkeyfile [<key>|--stdin]       Create keyfile. A random
       seed will be used if no <key> is provided on the command line
@@ -413,6 +431,8 @@ printHelp(
 
       Default keyfile is: )"
               << defaultKeyfile << "\n";
+    // In progress:
+    // hash <prefix> <argument>|--stdin             Hash an XRPL object.
 }
 
 InputType
